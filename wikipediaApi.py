8f86568b89d13ea02b128305af10ddfec90a59ed
TheArtisk <poubelle.rouge@laposte.net>
2019-12-09 18:17:42 +0100

    Help for wikipedia ambiguation error

--- wikipediaApi.py
+++ wikipediaApi.py
@@ -6,8 +6,11 @@ class wiki:
             s = wikipedia.page(str(input))
             print (f"Wiki search for {s.title}")
             return(wikipedia.summary(input,sentences = 2))
-        except DisambiguationError:
-            return("Disambiguation Error, search for something more precise.")
+        except wikipedia.exceptions.DisambiguationError as e: #Handling of disambiguation error from wikipedia api
+            e = str(e)
+            e = e.replace("\n",", ")
+
+            return(f"Disambiguation Error, search for something more precise. : {e}")
 
 if __name__ == "__main__":
-    wiki.WSearch("Barack")
+    print("\n" + wiki.WSearch("Mu"))
