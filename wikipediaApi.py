9ef4eae2b9e75a26035d197766d687a08533c52a
TheArtisk <poubelle.rouge@laposte.net>
2019-12-09 15:31:23 +0100

    Handdle of DisambiguationError for wikipedia api search

--- wikipediaApi.py
+++ wikipediaApi.py
@@ -2,9 +2,12 @@ import wikipedia
 
 class wiki:
     def WSearch(input):
-        s = wikipedia.page(str(input))
-        print (f"Wiki search for {s.title}")
-        return(wikipedia.summary(input,sentences = 2))
+        try :
+            s = wikipedia.page(str(input))
+            print (f"Wiki search for {s.title}")
+            return(wikipedia.summary(input,sentences = 2))
+        except DisambiguationError:
+            return("Disambiguation Error, search for something more precise.")
 
 if __name__ == "__main__":
     wiki.WSearch("Barack")
